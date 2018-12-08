894393fef0232ed3412ac36e0b621d4f5bf47ef3
hammeda <hammedaimennc@gmail.com>
2018-12-08 11:27:59 +0100

    correction bug

--- include/details_projects.php
+++ include/details_projects.php
@@ -29,6 +29,7 @@
   <section id="fond_projects">
       <img src="../images/forme-1.png" alt="forme bleu" width="auto" id="forme1">
       <img src="../images/forme-2.png" alt="forme bleu" width="auto" id="forme2">
+  </section>
   <header>
   			<div class="rec">
   			</div>
@@ -53,19 +54,19 @@
   					</div>
   				<div id= "listemenu2">
   						<ul id="listemenu3" class="r_30">
-  							<li date-image="chainazik.png"
+  							<li data-image="chainazik.png"
                     data-text1="Le festival Chaina’zik est un festival de musique qui se déroule tous les ans à Chainaz les Frasses en Savoie. Composé exclusivement de bénévole, notre client est Philippe Chappelet l’un des dirigeant de cet événement. Il est spécialisé dans la création et la gestion d’événement. Nous avons avant tout conversé avec le fils du client, Jules Chappelet, car il a de bonne base en audiovisuel étant donné qu’il a réalisé les aftermovies et films de plusieurs éditions de Chaina’zik."
                     data-text2="Ce festival nous a demandé de faire la captation vidéo de leur 9ème édition pour un aftermovie et un film dédié aux bénévoles pour les remerciés de leurs implications."> Chaina'zik </li>
-  							<li date-image="chainazik.png"
+  							<li data-image="chainazik.png"
                     data-text1="Notre cliente est Virginie Rodier, responsable de la communication de l’IUT de Chambéry. C’est une ancienne MMI (YEAAAAAH)."
                     data-text2="Pour la communication de l’IUT, nous nous occupons des posts sur les différents réseaux sociaux et des différents évènements dans l’enceinte de l’IUT comme la St Valentin, le Noël ou le Festival de l’IUT."> Com IUT </li>
-  							<li date-image="chainazik.png"
+  							<li data-image="chainazik.png"
                     data-text1="L’association AFS Vivre sans frontière pays de Savoie est une association loi 1901 qui permet de faire des voyages à l’étranger pour étudier la langue et la culture. Notre cliente est Bénédicte Virguerie, une professeure des écoles bénévole au sein de l’association. Elle aime la nature et les petits chatons <3"
                     data-text2="Elle nous a contacté car elle avait besoin d’une stratégie de communication pour ses réseaux sociaux. Et pour cela elle a demandé 2 vidéos courtes pour donner envie à certaines personnes d’accueillir de jeunes étudiants étrangers, ainsi que la création d’une page Facebook et d’un compte Instagram AFS Pays de Savoie accompagné de deux guides explicatifs sur le fonctionnement de ces derniers."> AFS </li>
-  							<li date-image="chainazik.png"
+  							<li data-image="chainazik.png"
                     data-text1="Notre cliente est une amie de la dirigeante du Poney-Club Chantemerle qui, à la suite d’un grave accident, se retrouve en grande difficulté pour s’occuper seule de son haras"
                     data-text2="Elle nous a contacté car elle avait besoin d’une stratégie de communication pour ses réseaux sociaux. Et pour cela elle a demandé 2 vidéos courtes pour donner envie à certaines personnes d’accueillir de jeunes étudiants étrangers, ainsi que la création d’une page Facebook et d’un compte Instagram AFS Pays de Savoie accompagné de deux guides explicatifs sur le fonctionnement de ces derniers."> Poney Club </li>
-  							<li date-image="chainazik.png"
+  							<li data-image="chainazik.png"
                     data-text1="Notre cliente est une amie de la dirigeante du Poney-Club Chantemerle qui, à la suite d’un grave accident, se retrouve en grande difficulté pour s’occuper seule de son haras"
                     data-text2="Elle nous a contacté car elle avait besoin d’une stratégie de communication pour ses réseaux sociaux. Et pour cela elle a demandé 2 vidéos courtes pour donner envie à certaines personnes d’accueillir de jeunes étudiants étrangers, ainsi que la création d’une page Facebook et d’un compte Instagram AFS Pays de Savoie accompagné de deux guides explicatifs sur le fonctionnement de ces derniers."> Efficience </li>
   						</ul>
@@ -76,11 +77,11 @@
 
 <div class= "rectangle">
   <div class="texte">
-    <img id="logo" src="" alt="">
+    <img id="logo" src="" >
     <ul id= "liste2" class="m_50">
       <li><a href="#centrerctxt"> CONTEXTE </a></li>
       <li><a href="#centrerdmd"> DEMANDE </a></li>
-      <li> LIVRABLES </li>
+      <li><a href="#centrerlivrables"> LIVRABLES </a></li>
     </ul>
     <div id="centrerctxt" class="m_50">
       <h1 class="contexte">CONTEXTE</h1>
@@ -93,10 +94,18 @@
 
         </p>
     </div>
+    <div id="centrerlivrables" class="m_50">
+        <h1 class="livrables">LIVRABLES</h1>
+        <div id="livr">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/tDMz9Qgnrxw"
+          frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope;
+          picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
 
   </div>
 </div>
-</section>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="../scripts/menu.js"></script>
 </body>
--- scripts/menu.js
+++ scripts/menu.js
@@ -22,7 +22,7 @@ projets();
 function toggleImage() {
 	$('ul#listemenu3 li').click(function() {
 		var image = $(this).attr('data-image');
-		$('img#logo').attr('src', 'images/'+image);
+		$('img#logo').attr('src', '../images/projets/'+image);
 
 		var text1 = $(this).attr('data-text1');
 		$('p#ctxt').html(text1);
@@ -32,3 +32,16 @@ function toggleImage() {
 	});
 }
 toggleImage();
+
+function initToggle() {
+	var image = $('ul#listemenu3 li:first-child').attr('data-image');
+	$('img#logo').attr('src', '../images/projets/'+image);
+
+	var text1 = $('ul#listemenu3 li:first-child').attr('data-text1');
+	$('p#ctxt').html(text1);
+
+	var text2 = $('ul#listemenu3 li:first-child').attr('data-text2');
+	$('p#dmd').html(text2);
+}
+
+initToggle();
